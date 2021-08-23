8fd29fed5e7e8876314d5ba2bf0e69061d6d336d
Djordje-Nikolic97 <djordje.nikolic0997@gmail.com>
2021-08-23 14:42:01 +0200

    Set up server-side signalr

---
+++ Groover/Groover.BL/Services/Interfaces/INotificationService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Groover.BL.Services.Interfaces
+{
+    public interface INotificationService
+    {
+        public Task UserLeftGroupAsync(int groupId, int userId);
+        public Task UserJoinedGroupAsync(int groupId, int userId);
+        public Task UserInvitedAsync(int groupId, int userId);
+        public Task UserRoleUpdatedAsync(int groupId, int userId);
+        public Task UserUpdatedAsync(int userId, List<int> groupIds);
+        public Task GroupUpdatedAsync(int groupId);
+        public Task GroupDeletedAsync(int groupId);
+    }
+}
---
+++ Groover/Groover.BL/Services/NotificationService.cs
@@ -0,0 +1,56 @@
+﻿using Groover.BL.Hubs;
+using Groover.BL.Services.Interfaces;
+using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Groover.BL.Services
+{
+    public class NotificationService : INotificationService
+    {
+        private readonly IHubContext<GroupChatHub> _hubContext;
+
+        public NotificationService(IHubContext<GroupChatHub> hubContext)
+        {
+            this._hubContext = hubContext;
+        }
+
+        public async Task UserLeftGroupAsync(int groupId, int userId)
+        {
+
+        }
+
+        public async Task UserJoinedGroupAsync(int groupId, int userId)
+        {
+
+        }
+
+        public async Task UserInvitedAsync(int groupId, int userId)
+        {
+
+        }
+
+        public async Task UserRoleUpdatedAsync(int groupId, int userId)
+        {
+
+        }
+
+        public async Task UserUpdatedAsync(int userId, List<int> groupIds)
+        {
+
+        }
+
+        public async Task GroupUpdatedAsync(int groupId)
+        {
+
+        }
+
+        public async Task GroupDeletedAsync(int groupId)
+        {
+
+        }
+    }
+}
