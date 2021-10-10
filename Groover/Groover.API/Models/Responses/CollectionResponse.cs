95b5c4985566ed2f0341ce9fa7be15b962ea26e8
Djordje-Nikolic97 <djordje.nikolic0997@gmail.com>
2021-10-10 02:27:09 +0200

    Added endpoints for pulling all messages

---
+++ Groover/Groover.API/Models/Responses/CollectionResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Groover.API.Models.Responses
+{
+    public class CollectionResponse<T>
+    {
+        public ICollection<T> Items { get; set; }
+    }
+}
--- Groover/Groover.BL/Models/Chat/DTOs/PageParamsDTO.cs
+++ Groover/Groover.BL/Models/Chat/DTOs/PageParamsDTO.cs
@@ -11,7 +11,7 @@ namespace Groover.BL.Models.Chat.DTOs
     {
         [Range(1, int.MaxValue, ErrorMessage = "Value for {0} must be between {1} and {2}.")]
         public int PageSize { get; set; }
-        public string PagingState { get; set; }
+        public string? PagingState { get; set; }
         public string NextPagingState { get; set; }
     }
 }
