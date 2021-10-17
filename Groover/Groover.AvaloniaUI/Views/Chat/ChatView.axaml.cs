179ae9dbd16ed88b9e972e43a17d66fe8d278e5c
Djordje-Nikolic97 <djordje.nikolic0997@gmail.com>
2021-10-17 03:34:54 +0200

    Temporary fix for auto scroll to bottom

--- Groover/Groover.AvaloniaUI/Views/Chat/ChatView.axaml.cs
+++ Groover/Groover.AvaloniaUI/Views/Chat/ChatView.axaml.cs
@@ -7,6 +7,9 @@ using Groover.AvaloniaUI.ViewModels.Chat;
 using ReactiveUI;
 using System.Reactive.Linq;
 using DynamicData;
+using System.Reactive.Disposables;
+using Avalonia.Threading;
+using System.Reactive;
 
 namespace Groover.AvaloniaUI.Views.Chat
 {
@@ -22,16 +25,41 @@ namespace Groover.AvaloniaUI.Views.Chat
 
             this.WhenActivated(disposables =>
             {
-                ViewModel?.InitializeCommand.Subscribe(_ =>
-                {
-                    _messageScrollViewer?.ScrollToEnd();
-                });
+                this.WhenAnyObservable(view => view.ViewModel.AddMessageCommand)
+                    .Delay(TimeSpan.FromMilliseconds(100))
+                    .Subscribe(_ =>
+                    {
+                        Dispatcher.UIThread.InvokeAsync(ScrollMessagesToEnd);
+                    })
+                    .DisposeWith(disposables);
+
+                //The goal of the following is to scroll to the bottom when the messages are first loaded
+                this.WhenAnyObservable(view => view.ViewModel.InitializeCommand)
+                    .Delay(TimeSpan.FromMilliseconds(100))
+                    .FirstAsync()
+                    .Subscribe(_ =>
+                    {
+                        Dispatcher.UIThread.InvokeAsync(ScrollMessagesToEnd);
+                    })
+                    .DisposeWith(disposables);
+
+                //The goal of the following is to scroll to the bottom when groups are switched
+                _messageScrollViewer.AttachedToVisualTree += async (s,e) => await Dispatcher.UIThread.InvokeAsync(ScrollMessagesToEnd);
+
             });
         }
 
         private void InitializeComponent()
         {
             AvaloniaXamlLoader.Load(this);
+        }
+
+        private void ScrollMessagesToEnd()
+        {
+            var tempVisib = _messageScrollViewer.VerticalScrollBarVisibility;
+            _messageScrollViewer.VerticalScrollBarVisibility = Avalonia.Controls.Primitives.ScrollBarVisibility.Visible;
+            _messageScrollViewer.ScrollToEnd();
+            _messageScrollViewer.VerticalScrollBarVisibility = tempVisib;
         }
     }
 }
